d7188f9fe61507d0ff7da25dd1c633b6f1e967bc
unknown <thyxaxa@gmail.com>
2022-01-22 11:24:58 +0200

    organization changes

--- old-wersion/js/script.js
+++ old-wersion/js/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const isMobile = {
         Android: function () {
             return navigator.userAgent.match(/Android/i);
@@ -42,114 +44,13 @@ const isMobile = {
         document.body.classList.add('_pc');
     }
 
-
-// 1 seconds
-// 1 minute = 60 seconds
-// 1 hour = 3 600 seconds
-// 1 day = 86 400 seconds
-// 1 year = 31 536 000
-
-function formatDuration (seconds) {
-    let start = seconds;
-    let second = 0;
-    let minute = 0;
-    let hour = 0;
-    let day = 0;
-    let year = 0;
-    let result = '';
-  
-    for(let index = 0; start > 0 ; index++){
-        if(start >= 31536000){
-            start -= 31536000;
-            year++;
-        } else {
-            break;
-        }
-    };
-  
-    for(let index = 0; start > 0 ; index++){
-        if(start >= 86400){
-            start -= 86400;
-            day++;
-        } else {
-            break;
-        }
-    };
-  
-    for(let index = 0; start > 0 ; index++){
-        if(start >= 3600){
-            start -= 3600;
-            hour++;
-        } else {
-            break;
-        }
-    };
-  
-    for(let index = 0; start > 0 ; index++){
-        if(start >= 60){
-            start -= 60;
-            minute++;
-        } else {
-            break;
-        }
-    };
-  
-    for(let index = 0; start > 0 ; index++){
-        if(start >= 1){
-            start -= 1;
-            second++;
-        } else {
-            break;
-        }
-    };
-  
-    if(year > 0){
-      
-      result = `${year} year,`;
-      
-      if(year > 1){
-        result = `${year} years,`;
-      }  
-    }
-  
-    if(day > 0){
-      let temporaryLet = (day > 1) ? 'days' : 'day';
-  
-      result = (year != 0) ? result + ` ${day} ${temporaryLet}` : result + `${day} ${temporaryLet}`;    
-    }
     
-    if(hour > 0){
-      let temporaryLet = (hour > 1) ? 'hours' : 'hour';
-  
-      result = (day != 0) ? result + `, ${hour} ${temporaryLet}` : result + `${hour} ${temporaryLet}`;
-    }
-  
-    if(minute > 0){
-      let temporaryLet = (minute > 1) ? 'minutes' : 'minute';
-  
-      result = (hour != 0) ? result + `, ${minute} ${temporaryLet}` : result + `${minute} ${temporaryLet}` ;
-      
-      if(hour == 0){
-        result = result.split(' ');
-        result[1] = `days, ${minute}`;
-        result = result.join(' ')
-      }
-    }
-  
-    if(second > 0){
-      let temporaryLet = (second > 1) ? 'seconds' : 'second';
-      
-      result = (minute != 0) ? result + ` and ${second} ${temporaryLet}` : result + `${second} ${temporaryLet}` ;
-    }
-    
-    console.log(result);
-    // console.log(result[1]);
-    return result;
-  }
-    // console.log(year)
-    // console.log(day)
-    // console.log(hour)
-    // console.log(minute)
-    // console.log(second)
-  
-  formatDuration (777721);
+const firstNavbar = document.querySelector('.first-navbar');
+const secondNavbar = document.querySelector('.second-navbar');
+
+setTimeout(function(){
+    firstNavbar.classList.add('--displayNone');
+    secondNavbar.classList.add('--displayFlex');
+}, 2000)
+
+console.log(firstNavbar);
