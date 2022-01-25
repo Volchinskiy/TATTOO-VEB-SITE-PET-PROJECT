1b2858a0036bd361c9eb8c719a1d08919b27a6d5
unknown <thyxaxa@gmail.com>
2022-01-25 15:47:31 +0200

    added a font and completely finished the menu logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import Header from './сomponents/Header';
 
+setTimeout(function(){
+  const firstNavbar = document.querySelector('.first-navbar');
+  const menuButton = document.querySelector('.header__memu-button');
+  
+  firstNavbar.classList.add('--opacityZero');
+  menuButton.classList.remove('--displayNone');
 
+  setTimeout(function(){
+    firstNavbar.classList.add('--displayNone');
+    menuButton.classList.remove('--opacityZero');
 
-const firstNavbar = document.querySelector('.first-navbar');
-const secondNavbar = document.querySelector('.second-navbar');
+  }, 1000);
+}, 2000)
 
-console.log(firstNavbar);
-
-// setTimeout(function(){
-//     firstNavbar.classList.add('--displayNone');
-//     secondNavbar.classList.add('--displayFlex');
-// }, 2000)
 
 function App() {
   return (
     <div>
-      <Header name="я новй пропс" />      
+      <Header />
     </div>
   );
 }
--- src/сomponents/Header.js
+++ src/сomponents/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FirstNavBar from './FirstNavBar';
 import SecondNavBar from './SecondNavBar';
+import MenuButton from './MenuButton';
 
-export default function Header(props) {
+
+export default function Header() {
   return <header>
             <FirstNavBar />
-                {props.name}
             <SecondNavBar />
+            <MenuButton />
          </header>;
 }
